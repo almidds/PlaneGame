896f1a4c5cf4501addc09fd434a4a11e3448fa5a
almidds <almidds@hotmail.com>
2024-05-18 00:19:46 +0100

    work

--- Assets/Editor/ChunkEditor.cs
+++ Assets/Editor/ChunkEditor.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEditor;
+
 using UnityEngine;
 
-[CustomEditor (typeof(Chunk))]
-public class ChunkEditor : Editor {
+[CustomEditor(typeof(Chunk))]
+public class ChunkEditor : Editor
+{
     public override void OnInspectorGUI()
     {
         Chunk chunk = (Chunk)target;
-        if (DrawDefaultInspector()) {
+        if (DrawDefaultInspector())
+        {
             chunk.GenerateMap();
         }
-        if (GUILayout.Button("Generate")) {
+        if (GUILayout.Button("Generate"))
+        {
             chunk.GenerateMap();
         }
     }
