c11838f3baefd5a31ef82eb760c6827950a11d58
almidds <almidds@hotmail.com>
2024-05-23 21:54:43 +0100

    #2: improve noise generation (#4)
    * Rework noise generation
    
    * Rework files
    
    * linting

--- Assets/Scripts/NoiseGenerator.cs
+++ Assets/Scripts/NoiseGenerator.cs
@@ -2,6 +2,8 @@ using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 
+using UnityEditor;
+
 using UnityEngine;
 
 public class NoiseGenerator : MonoBehaviour
@@ -12,17 +14,19 @@ public class NoiseGenerator : MonoBehaviour
     [SerializeField]
     private ComputeShader noiseGenerator;
 
-    [Header("Noise")]
-    public int seed;
-    public int numOctaves = 4;
-    public float noiseScale = 1;
-    public float lacunarity = 2;
-    public float gain = 0.5f;
-    public float amplitude = 1;
 
-
-    public float[] GenerateNoise(Vector3 position)
+    public float[] GenerateNoise(Vector3 position, int seed, int numOctaves, float noiseScale, float lacunarity, float gain, float floorOffset, float hardFloor, float hardFloorWeight, float weightMultiplier, float noiseWeight, Vector4 shaderParams)
     {
+        var prng = new System.Random(seed);
+        var offsets = new Vector3[numOctaves];
+        float offsetRange = 1000;
+        for (int i = 0; i < numOctaves; i++)
+        {
+            offsets[i] = new Vector3((float)prng.NextDouble() * 2 - 1, (float)prng.NextDouble() * 2 - 1, (float)prng.NextDouble() * 2 - 1) * offsetRange;
+        }
+        var offsetsBuffer = new ComputeBuffer(offsets.Length, sizeof(float) * 3);
+        offsetsBuffer.SetData(offsets);
+
         _weightsBuffer = new ComputeBuffer(
             TerrainMetrics.PointsPerChunk *
             TerrainMetrics.PointsPerChunk *
@@ -30,17 +34,23 @@ public class NoiseGenerator : MonoBehaviour
             sizeof(float)
         );
         noiseGenerator.SetBuffer(0, "_weights", _weightsBuffer);
+        noiseGenerator.SetBuffer(0, "offsets", offsetsBuffer);
 
         noiseGenerator.SetInt("numPointsPerAxis", TerrainMetrics.PointsPerChunk);
         noiseGenerator.SetFloat("chunkSize", TerrainMetrics.ChunkSize);
         noiseGenerator.SetVector("chunkCentre", position);
 
-        noiseGenerator.SetInt("seed", seed);
-        noiseGenerator.SetInt("octaves", numOctaves);
+        noiseGenerator.SetInt("numOctaves", numOctaves);
         noiseGenerator.SetFloat("noiseScale", noiseScale);
         noiseGenerator.SetFloat("lacunarity", lacunarity);
         noiseGenerator.SetFloat("gain", gain);
-        noiseGenerator.SetFloat("amplitude", amplitude);
+        noiseGenerator.SetFloat("weightMultiplier", weightMultiplier);
+        noiseGenerator.SetFloat("noiseWeight", noiseWeight);
+        noiseGenerator.SetVector("params", shaderParams);
+
+        noiseGenerator.SetFloat("floorOffset", floorOffset);
+        noiseGenerator.SetFloat("hardFloor", hardFloor);
+        noiseGenerator.SetFloat("hardFloorWeight", hardFloorWeight);
 
 
         noiseGenerator.Dispatch(0,
@@ -52,6 +62,8 @@ public class NoiseGenerator : MonoBehaviour
                                         TerrainMetrics.PointsPerChunk];
         _weightsBuffer.GetData(noiseValues);
         _weightsBuffer.Release();
+        offsetsBuffer.Release();
+
         return noiseValues;
     }
 }
